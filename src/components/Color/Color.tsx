0b75d0195910bc94fdfcc4de0d64d32d4bc6a66d
christinaminh <christina.minh@gmail.com>
2021-02-03 12:58:59 -0800

    Parsed through response for image submit to vision API

---
+++ src/components/Color/Color.tsx
@@ -0,0 +1,17 @@
+ import React from 'react';
+
+// interface colorProps {
+//   red: number,
+//   green: number,
+//   blue: number,
+//   score: number
+// }
+
+const Color = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default Color
+
---
+++ src/components/Color/ColorPalette.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// interface Props {
+//   colors: [{}]
+// }
+
+const ColorPalette = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default ColorPalette
+
