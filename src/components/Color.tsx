5a1f632d4278254eac0c164e19ec0407b57ad0b6
christinaminh <christina.minh@gmail.com>
2021-02-17 18:41:34 -0800

    Changed classname of checkmark for color palette item since it was not showing up on netlify

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -40,8 +40,8 @@ const Color: React.FC<ColorProps & ColorCallback> = ({...props}) => {
 
 
   return(
-      <div className={selected ? 'selected color-item': 'color-item '} style={colorItemStyle} onClick={handleClickedColor}>
-        <img src={checkmark} alt='select color' className={selected ? 'checked' : 'unchecked'}/>
+      <div className={selected ? 'selected color-item': 'color-item'} style={colorItemStyle} onClick={handleClickedColor}>
+        <img src={checkmark} alt='select color' className={selected ? 'checked-color-item' : 'unchecked-color-item'}/>
 
       </div>
   )
